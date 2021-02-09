6881ccca5996e4e419ce4a1c08bc5876168773f5
POWER <clay.degruchy@gmail.com>
2021-02-09 13:46:08 +0100

    remmoved junk

--- potion-gen.js
+++ potion-gen.js
@@ -645,24 +645,24 @@ function tests(seed = 1) {
 // tests()
 // console.log(x)
 
-var x = require('fs').readFileSync("rename-ingredients.json")
-x = JSON.parse(x)
+// var x = require('fs').readFileSync("rename-ingredients.json")
+// x = JSON.parse(x)
 
-console.log(og.ingredients.length)
-console.log(x.length)
+// console.log(og.ingredients.length)
+// console.log(x.length)
 
-x.map((ing, i) => {
-    ing.originalName = og.ingredients[i].name
-    return ing
-})
+// x.map((ing, i) => {
+//     ing.originalName = og.ingredients[i].name
+//     return ing
+// })
 
-x = x.sort(function(a, b) {
-    if (a.name < b.name) { return -1; }
-    if (a.name > b.name) { return 1; }
-    return 0;
-})
-// console.log(x)
-require('fs').writeFileSync("rename-ingredients.json", JSON.stringify(x, null, 2))
+// x = x.sort(function(a, b) {
+//     if (a.name < b.name) { return -1; }
+//     if (a.name > b.name) { return 1; }
+//     return 0;
+// })
+// // console.log(x)
+// require('fs').writeFileSync("rename-ingredients.json", JSON.stringify(x, null, 2))
 
 
 
